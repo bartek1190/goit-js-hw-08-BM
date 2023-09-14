47f5c467a7e4afb0dd71f80d2e87765cac709817
bartek1190 <bartlomiej.m.michonski@gmail.com>
2023-09-14 14:20:55 +0200

    exer 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+    </a>
+  `;
+};
+
+const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+});
